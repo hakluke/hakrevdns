ec646e8bc4648d0fa3218cec63b26525d380a1be
Alphakilo <Alphakilo@users.noreply.github.com>
2020-01-11 10:50:38 +0100

    declare net.Resolver once

--- main.go
+++ main.go
@@ -20,26 +20,21 @@ var opts struct {
 func worker(ip string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
+	var r *net.Resolver
+
 	if opts.ResolverIP != "" {
-		r := &net.Resolver{
+		r = &net.Resolver{
 			PreferGo: true,
 			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
 				d := net.Dialer{}
 				return d.DialContext(ctx, opts.Protocol, fmt.Sprintf("%s:%d", opts.ResolverIP, opts.Port))
 			},
 		}
+	}
 
-		addr, err := r.LookupAddr(context.Background(), ip)
-		if err == nil {
-			fmt.Printf("%s\t%s\n", ip, addr[0])
-		} else {
-			fmt.Println(err)
-		}
-	} else {
-		addr, err := net.LookupAddr(ip)
-		if err == nil {
-			fmt.Printf("%s\t%s\n", ip, addr[0])
-		}
+	addr, err := r.LookupAddr(context.Background(), ip)
+	if err == nil {
+		fmt.Printf("%s\t%s\n", ip, addr[0])
 	}
 }
 
