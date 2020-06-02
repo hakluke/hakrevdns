570feda21ed03c9708034dc4bb9b1860999c34ae
hackerpain <61242234+hackerpain@users.noreply.github.com>
2020-06-02 15:17:54 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,10 +7,7 @@ import (
     "fmt"
     "net"
     "sync"
-<<<<<<< master
     "strings"
-=======
->>>>>>> master
     "os"
 )
 
@@ -19,10 +16,7 @@ var opts struct {
         ResolverIP string `short:"r" long:"resolver" description:"IP of the DNS resolver to use for lookups"`
         Protocol   string `short:"P" long:"protocol" choice:"tcp" choice:"udp" default:"udp" description:"Protocol to use for lookups"`
         Port       uint16 `short:"p" long:"port" default:"53" description:"Port to bother the specified DNS resolver on"`
-<<<<<<< master
 	Domain     bool   `short:"d" long:"domain" description:"Output only domains"`
-=======
->>>>>>> master
 }
 
 func main() {
@@ -73,15 +67,11 @@ func doWork(work chan string, wg *sync.WaitGroup) {
         }
 
         for _, a := range addr {
-<<<<<<< master
 		if opts.Domain {
 			fmt.Println(strings.TrimRight(a, "."))
 		} else {
                 	fmt.Println(ip, "\t",a)
 		}
-=======
-                fmt.Println(ip, "\t",a)
->>>>>>> master
         }
     }
 }
